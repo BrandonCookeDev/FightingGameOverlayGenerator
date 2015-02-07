9b783425cbdd546c85fdf64a77e02ac570034703
Brandon Cooke <BCooke91@Gmail.com>
2015-02-06 23:29:29 -0500

    Revert "Revert "Initial Commit:""
    This reverts commit 4ebcf90062fdc076ae73b2f6ac788b839dd419a5.

---
+++ SmashOverlayGeneratorMk2/SmashOverlayGeneratorMk2/Objects/Template/CompetitorTemplate.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using SmashOverlayGeneratorMk2.Objects.Points;
+
+namespace SmashOverlayGeneratorMk2.Objects
+{
+    class CompetitorTemplate : Template
+    {
+        private CompetitorPoint competitor1Point;
+        private CompetitorPoint competitor2Point;
+        private CompetitorPoint competitor1WebcamPoint;
+        private CompetitorPoint competitor2WebcamPoint;
+        private ScorePoint score1Point;
+        private ScorePoint score2Point;
+        private TournamentPoint tournamentNameRoundPoint;
+
+
+        //DETERMINE FONT SIZE
+        private Font name1Font;
+        private Font name2Font;
+        private Font scoreFont;
+        private Font tournmentFont;                 
+
+        public CompetitorTemplate(string filePath) : base(filePath) { }
+
+        public CompetitorTemplate(string filePath,
+                                  CompetitorPoint competitor1, 
+                                  CompetitorPoint competitor2,
+                                  ScorePoint score1, ScorePoint score2,
+                                  TournamentPoint tournamentNameRound)
+            : base(filePath)
+        {
+            if(competitor1 == null)
+               throw new Exception("Competitor Template: Competitor 1 must have a point on template");
+            if(competitor2 == null)
+                throw new Exception("Competitor Template: Competitor 2 must have a point on template");
+            if (score1 == null)
+                throw new Exception("Competitor Template: Score 1 must have a point on template");
+            if (score2 == null)
+                throw new Exception("Competitor Template: Score 2 must have a point on template");
+            if (tournamentNameRound == null)
+                throw new Exception("Competitor Template: Tournament Name must have a point on template");
+          
+            this.competitor1Point = competitor1;
+            this.competitor2Point = competitor2;
+            this.score1Point = score1;
+            this.score2Point = score2;
+            this.tournamentNameRoundPoint = tournamentNameRound;
+        }
+
+        public CompetitorTemplate(string filePath,
+                                 CompetitorPoint competitor1,
+                                 CompetitorPoint competitor2,
+                                 CompetitorPoint competitor1Webcam,
+                                 CompetitorPoint competitor2Webcam,
+                                 ScorePoint score1, ScorePoint score2,
+                                 TournamentPoint tournamentNameRound)
+            : base(filePath)
+        {
+            if (competitor1 == null)
+                throw new Exception("Competitor Template: Competitor 1 must have a point on template");
+            if (competitor2 == null)
+                throw new Exception("Competitor Template: Competitor 2 must have a point on template");
+            if (competitor1Webcam == null)
+                throw new Exception("Competitor Template: Competitor 1's Webcam must have a point on template");
+            if (competitor2Webcam == null)
+                throw new Exception("Competitor Template: Competitor 2's Webcam must have a point on template");
+            if (score1 == null)
+                throw new Exception("Competitor Template: Score 1 must have a point on template");
+            if (score2 == null)
+                throw new Exception("Competitor Template: Score 2 must have a point on template");
+            if (tournamentNameRound == null)
+                throw new Exception("Competitor Template: Tournament Name must have a point on template");
+          
+            this.competitor1Point = competitor1;
+            this.competitor2Point = competitor2;
+            this.competitor1WebcamPoint = competitor1Webcam;
+            this.competitor2WebcamPoint = competitor2Webcam;
+            this.score1Point = score1;
+            this.score2Point = score2;
+            this.tournamentNameRoundPoint = tournamentNameRound;
+        }
+
+        public CompetitorPoint Competitor1Point
+        {
+            get { return this.competitor1Point; }
+            set { this.competitor1Point = value; }
+        }
+
+        public CompetitorPoint Competitor2Point
+        {
+            get { return this.competitor2Point; }
+            set { this.competitor2Point = value; }
+        }
+
+        public CompetitorPoint Competitor1WebcamPoint
+        {
+            get { return this.competitor1WebcamPoint; }
+            set { this.competitor1WebcamPoint = value; }
+        }
+
+        public CompetitorPoint Competitor2WebcamPoint
+        {
+            get { return this.competitor2WebcamPoint; }
+            set { this.competitor2WebcamPoint = value; }
+        }
+
+        public ScorePoint Score1Point
+        {
+            get { return this.score1Point; }
+            set { this.score1Point = value; }
+        }
+
+        public ScorePoint Score2Point
+        {
+            get { return this.score2Point; }
+            set { this.score2Point = value; }
+        }
+
+        public TournamentPoint TournamentNameRoundPoint
+        {
+            get { return this.tournamentNameRoundPoint; }
+            set { this.tournamentNameRoundPoint = value; }
+        }
+
+        public Font Name1Font
+        {
+            get { return this.name1Font; }
+            set { this.name1Font = value; }
+        }
+
+        public Font Name2Font
+        {
+            get { return this.name2Font; }
+            set { this.name2Font = value; } 
+        }
+
+        public Font ScoreFont
+        {
+            get { return this.scoreFont; }
+            set { this.scoreFont = value; }
+        }
+
+        public Font TournamentFont
+        {
+            get { return this.tournmentFont; }
+            set { this.tournmentFont = value; }
+        }
+
+        public void setFontSizes(string resourceType, 
+                                 string competitor1, string competitor2)
+        {
+            int[] fontSize = determineFontSize(competitor1, competitor2);
+            if (resourceType.Equals("file"))
+            {
+                Name1Font = new Font(FontFamily.GenericSansSerif, fontSize[0], FontStyle.Bold);
+                Name2Font = new Font(FontFamily.GenericSansSerif, fontSize[1], FontStyle.Bold);
+                ScoreFont = new Font(FontFamily.GenericSansSerif, 45, FontStyle.Bold);
+                TournamentFont = new Font(FontFamily.GenericSansSerif, 27, FontStyle.Bold);
+            }
+            else
+            {
+                Name1Font = new Font(FontFamily.GenericSansSerif, fontSize[0] + 8, FontStyle.Bold);
+                Name2Font = new Font(FontFamily.GenericSansSerif, fontSize[1] + 8, FontStyle.Bold);
+                ScoreFont = new Font(FontFamily.GenericSansSerif, 35, FontStyle.Bold);
+                TournamentFont = new Font(FontFamily.GenericSansSerif, 27, FontStyle.Bold);
+            }
+        }
+
+        public int[] determineFontSize(string competitor1, 
+                                                string competitor2)
+        {
+            int[] fontSizeArr = new int[2];
+            int MAX = 22, MEDIUM = 19, SMALL = 17, SMALL_PLUS = 15;
+
+            if (competitor1.Length >= 23) fontSizeArr[0] = SMALL_PLUS;
+            else if (competitor1.Length >= 20) fontSizeArr[0] = SMALL;
+            else if (competitor1.Length >= 17) fontSizeArr[0] = MEDIUM;
+            else fontSizeArr[0] = MAX;
+
+            if (competitor2.Length >= 23) fontSizeArr[1] = SMALL_PLUS;
+            else if (competitor2.Length >= 20) fontSizeArr[1] = SMALL;
+            else if (competitor2.Length >= 17) fontSizeArr[1] = MEDIUM;
+            else fontSizeArr[1] = MAX;
+
+            return fontSizeArr;
+        }
+
+        public override Bitmap drawTextOnImage(SmashOverlayGenerator form)
+        {
+            setFontSizes(form.ResourceType, form.Competitor1, form.Competitor2);
+
+            //CENTER NAME TEXT
+            StringFormat nameFormat = new StringFormat();
+            StringFormat scoreFormat = new StringFormat();
+
+            nameFormat.LineAlignment = StringAlignment.Center;
+            nameFormat.Alignment = StringAlignment.Center;
+            scoreFormat.Alignment = StringAlignment.Center;
+
+            Bitmap image = base.getImage();
+            Graphics g = Graphics.FromImage(image);
+
+            g.DrawString(form.Competitor1, Name1Font, Brushes.White, Competitor1Point.getPoint(), nameFormat);
+            g.DrawString(form.Competitor2, Name2Font, Brushes.White, Competitor2Point.getPoint(), nameFormat);
+            g.DrawString(form.Competitor1, Name1Font, Brushes.White, Competitor1WebcamPoint.getPoint(), nameFormat);
+            g.DrawString(form.Competitor2, Name2Font, Brushes.White, Competitor2WebcamPoint.getPoint(), nameFormat);
+            g.DrawString(form.Score1, ScoreFont, Brushes.White, Score1Point.getPoint(), scoreFormat);
+            g.DrawString(form.Score2, ScoreFont, Brushes.White, Score2Point.getPoint(), scoreFormat);
+            g.DrawString(form.TournamentName + " | " + form.TournamentRound, 
+                TournamentFont, Brushes.White, TournamentNameRoundPoint.getPoint(), nameFormat);
+
+            return image;
+        }
+
+        public override void saveImage(Bitmap image)
+        {
+            String fileName = base.FilePath;
+            fileName = fileName.Substring(fileName.LastIndexOf(".Templates.") + 11);
+            fileName = fileName.Substring(0, fileName.Length - 4) + "_official.png";
+            image.Save(fileName);
+        }
+    }
+}
---
+++ SmashOverlayGeneratorMk2/SmashOverlayGeneratorMk2/XSplit/Broadcaster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XSplitBroadcasterLib;
+namespace SmashOverlayGeneratorMk2.XSplit
+{
+    class Broadcaster : XSplitBroadcaster
+    {
+        public Broadcaster() { }
+    }
+}
